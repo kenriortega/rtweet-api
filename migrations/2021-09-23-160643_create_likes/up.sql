03366f1b0e9adaba4fed63cf7899adab35d87365
kenriortega <kenriortega@gmail.com>
2021-09-26 23:18:44 -0400

    ref: add on delete cascade forenkey

--- migrations/2021-09-23-160643_create_likes/up.sql
+++ migrations/2021-09-23-160643_create_likes/up.sql
@@ -1,7 +1,7 @@
 -- Your SQL goes here
-CREATE TABLE IF NOT EXISTS likes
+CREATE TABLE IF NOT EXISTS likes 
 (
     id         UUID PRIMARY KEY        NOT NULL,
     created_at TIMESTAMP DEFAULT now() NOT NULL,
-    tweet_id   UUID                    NOT NULL REFERENCES tweets (id)
+    tweet_id   UUID                    NOT NULL REFERENCES tweets (id) ON DELETE CASCADE
 );
