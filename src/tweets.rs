ee7741c005fe39668aec1339db4c233b88526e47
kenriortega <kenriortega@gmail.com>
2021-09-26 23:20:12 -0400

    feat: add delete & get by id methods

--- src/tweets.rs
+++ src/tweets.rs
@@ -8,10 +8,10 @@ use diesel::r2d2::{ConnectionManager, Pool};
 use diesel::PgConnection;
 use diesel::{ExpressionMethods, Insertable, Queryable, RunQueryDsl};
 use serde::{Deserialize, Serialize};
+use std::str::FromStr;
 use uuid::Uuid;
-
-#[table_name = "tweets"]
 #[derive(Queryable, Insertable, Serialize, Deserialize)]
+#[table_name = "tweets"]
 struct Tweet {
     id: Uuid,
     created_at: NaiveDateTime,
@@ -32,7 +32,7 @@ impl Tweet {
 #[get("/tweets")]
 pub async fn get_tweets(pool: Data<Pool<ConnectionManager<PgConnection>>>) -> HttpResponse {
     use crate::schema::tweets::dsl::*;
-    let conn = pool.get().expect("Error to insert");
+    let conn = pool.get().expect("Can`t connect to the DB");
     let result = tweets
         .order(created_at.desc())
         .limit(3)
@@ -53,7 +53,7 @@ pub async fn post_tweet(
     pool: Data<Pool<ConnectionManager<PgConnection>>>,
 ) -> HttpResponse {
     let new_tweet = Tweet::new(req_body);
-    let conn = pool.get().expect("Error to insert");
+    let conn = pool.get().expect("Can`t connect to the DB");
     diesel::insert_into(tweets::table)
         .values(&new_tweet)
         .execute(&conn)
@@ -64,8 +64,25 @@ pub async fn post_tweet(
 }
 // /tweets/:id GET: get tweet by id
 #[get("/tweets/{id}")]
-pub async fn get_tweet_by_id(path: Path<(String,)>) -> HttpResponse {
-    let tweet = format!("tweet: {:?}", path.0);
+pub async fn get_tweet_by_id(
+    path: Path<(String,)>,
+    pool: Data<Pool<ConnectionManager<PgConnection>>>,
+) -> HttpResponse {
+    use crate::schema::tweets::dsl::*;
+    let t_id = &path.0 .0; // tweet id desde los parametros de la url
+    let t_id_uuid = Uuid::from_str(t_id); // tweet id formateado a uuid
+
+    if t_id_uuid.is_err() {
+        println!("invalid tweet id, error: {:?}", t_id_uuid.err());
+        // si no pudimos convertir a un uuid válido, asumimos que el tweet no existe.
+        return HttpResponse::NotFound().await.unwrap();
+    }
+
+    let conn = pool.get().expect("Can`t connect to the DB");
+
+    let result = tweets.find(t_id_uuid.unwrap()).first::<Tweet>(&conn);
+    let tweet = result.expect("Not found");
+
     HttpResponse::Ok()
         .content_type(constants::APPLICATION_JSON)
         .json(tweet)
@@ -73,9 +90,26 @@ pub async fn get_tweet_by_id(path: Path<(String,)>) -> HttpResponse {
 
 // /tweets/:id DELETE: delete tweet by id
 #[delete("/tweets/{id}")]
-pub async fn delete_tweet_by_id(path: Path<(String,)>) -> HttpResponse {
-    let tweet = format!("tweet: {:?}", path.0);
+pub async fn delete_tweet_by_id(
+    path: Path<(String,)>,
+    pool: Data<Pool<ConnectionManager<PgConnection>>>,
+) -> HttpResponse {
+    use crate::schema::tweets::dsl::*;
+    let t_id = &path.0 .0; // tweet id desde los parametros de la url
+    let t_id_uuid = Uuid::from_str(t_id); // tweet id formateado a uuid
+
+    if t_id_uuid.is_err() {
+        println!("invalid tweet id, error: {:?}", t_id_uuid.err());
+        // si no pudimos convertir a un uuid válido, asumimos que el tweet no existe.
+        return HttpResponse::NotFound().await.unwrap();
+    }
+    let conn = pool.get().expect("Can`t connect to the DB");
+
+    let num_deleted = diesel::delete(tweets.filter(id.eq(t_id_uuid.unwrap())))
+        .execute(&conn)
+        .expect("Error deleting tweet");
+
     HttpResponse::Ok()
         .content_type(constants::APPLICATION_JSON)
-        .json(tweet)
+        .json(num_deleted)
 }
